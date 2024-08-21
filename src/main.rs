51ba7e8948541fe67a745e0358972fffae52f7e1
Venge M2 MBP <olegakbarov@gmail.com>
2024-08-20 22:10:55 -0700

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use protobuf_to_zod::parser::parse_proto_file;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    let proto_content =
-        fs::read_to_string("../files/simple.proto").expect("Failed to read the proto file");
+    let mut proto_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    proto_path.push("files");
+    proto_path.push("simple.proto");
+
+    let proto_content = fs::read_to_string(proto_path).expect("Failed to read the proto file");
 
     let _proto_file = parse_proto_file(&proto_content).expect("Failed to parse Protobuf file");
 
